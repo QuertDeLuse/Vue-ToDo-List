86bc7c2f3eb9d3eb9830d0937065f20621eeac8e
Quert_De_Luse <pragmavertigo@gmail.com>
2019-11-23 00:13:52 +0300

    Visual representation and some changes in program logic

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 Vue.component('todo-item', {
   props: ['todo'],
-  template: '<li>{{ todo.text }}</li>'
+  template: '<li class="list-item">'+
+      '<span class="navbar__id li__item">{{todo.id}}</span>'+
+      '<span class="navbar__descr li__item">{{ todo.text }}</span>'+
+    '</li>'
 });
 
 var todoApp = new Vue({
   el: '#todoApp',
   data: {
-    productList: [
-      { id: 0, text: 'q' },
-      { id: 1, text: 'w' },
-      { id: 2, text: 'e' }
+    todoList: [
+      { id: 0, text: 'sthnrt' },
+      { id: 1, text: 'xrtfjh' },
+      { id: 2, text: 'ertjhrt' }
     ]
+  },
+
+  methods: {
+    addTodoItem: function (obj) {      
+      if(obj == 1) {
+        todoApp.todoList.unshift({id: -1, text: 'fgnf'});
+      } 
+      if(obj == 'w') {
+        todoApp.todoList.push({id: 5, text: 'fgetjrtnnf'});
+      }   
+    }
   }
 });
 
